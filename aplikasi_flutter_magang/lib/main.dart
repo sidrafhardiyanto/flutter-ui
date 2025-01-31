cf9ff456a99b469b699b81cdb3708247f53bf561
Sidra Febrian Hardiyanto <sidrafhardiyanto@gmail.com>
2025-01-31 11:18:29 +0700

    menghapus column dan menggantinya menjadi container

--- aplikasi_flutter_magang/lib/main.dart
+++ aplikasi_flutter_magang/lib/main.dart
@@ -95,31 +95,18 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Klik tombol "+" untuk membuat aplikasi ini tertawa.\nAplikasi ini sudah tertawa:',
-            ),
-            Text(
-              '$_counter kali',
-              style: Theme.of(context).textTheme.headlineMedium,
+      body: Container(
+        height: 300,
+        width: double.infinity,
+        margin: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: Colors.grey,
+          borderRadius: BorderRadius.circular(20),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.2),
+              blurRadius: 15.0,
+              offset: Offset.zero,
             ),
           ],
         ),
