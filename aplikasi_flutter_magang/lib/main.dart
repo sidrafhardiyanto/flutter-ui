ee6c819ffa7564c061c9d8e5fd504173bd192269
Sidra Febrian Hardiyanto <sidrafhardiyanto@gmail.com>
2025-01-31 14:56:14 +0700

    color

--- aplikasi_flutter_magang/lib/main.dart
+++ aplikasi_flutter_magang/lib/main.dart
@@ -147,6 +147,65 @@ class CustomListView extends StatelessWidget {
   }
 }
 
+class MyWrap extends StatelessWidget {
+  MyWrap({super.key});
+
+  final List<Color> colors = [
+    Colors.red,
+    Colors.green,
+    Colors.blue,
+    Colors.yellow,
+    Colors.orange,
+    Colors.purple,
+    Colors.pink,
+    Colors.teal,
+    Colors.cyan,
+    Colors.indigo,
+    Colors.brown,
+    Colors.grey,
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Coding Flutter - Wrap"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8),
+        child: Column(
+          children: [
+            Wrap(
+              spacing: 20,
+              runSpacing: 10,
+              children: colors.map((color) {
+                return InkWell(
+                  onTap: () {},
+                  child: Container(
+                    width: 45,
+                    height: 45,
+                    decoration: BoxDecoration(
+                      border: Border.all(width: 3, color: Colors.grey),
+                      color: color,
+                      shape: BoxShape.circle,
+                      boxShadow: [
+                        BoxShadow(
+                            color: Colors.black.withOpacity(0.1),
+                            blurRadius: 15,
+                            offset: Offset.zero),
+                      ],
+                    ),
+                  ),
+                );
+              }).toList(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
 class _MyHomePageState extends State<MyHomePage> {
   // int _counter = 0;
 
@@ -193,10 +252,11 @@ class _MyHomePageState extends State<MyHomePage> {
         padding: const EdgeInsets.all(10),
         child: Column(
           children: [
-            // SizedBox(
-            //   height: 300,
+            SizedBox(
+              height: 300,
+              child: MyWrap(),
             //   child: MyListView(),
-            // ),
+            ),
             Text(
               "Discover the most modern furniture",
               textAlign: TextAlign.center,
