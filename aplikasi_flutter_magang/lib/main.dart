04b9ef98404318d0a3c2cbcdd95a238aa9cb37f3
Sidra Febrian Hardiyanto <sidrafhardiyanto@gmail.com>
2025-01-31 14:58:40 +0700

    dropdown

--- aplikasi_flutter_magang/lib/main.dart
+++ aplikasi_flutter_magang/lib/main.dart
@@ -234,6 +234,68 @@ class MyWrap extends StatelessWidget {
   }
 }
 
+class MyDropDown extends StatefulWidget {
+  const MyDropDown({super.key});
+
+  @override
+  State<MyDropDown> createState() => _MyDropDownState();
+}
+
+class _MyDropDownState extends State<MyDropDown> {
+  String selected = "Dart";
+
+  final List<String> dropDownList = const [
+    "Dart",
+    "Kotlin",
+    "Java",
+    "Javascript",
+    "PHP",
+    "Python",
+    "Ruby",
+    "Swift",
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        const Text("Bahasa favorit: "),
+        const SizedBox(width: 8),
+        DropdownButton(
+          value: selected,
+          icon: const Icon(Icons.arrow_drop_down),
+          iconSize: 20,
+          style: TextStyle(color: Colors.blue[600]),
+          underline: Container(
+            decoration: const BoxDecoration(
+              border: Border(
+                bottom: BorderSide(
+                  color: Colors.grey,
+                  width: 3,
+                ),
+              ),
+            ),
+          ),
+          items: dropDownList.map((String value) {
+            return DropdownMenuItem(
+              value: value,
+              child: Text(value),
+            );
+          }).toList(),
+          onChanged: (val) {
+            setState(() {
+              if (val != null) selected = val;
+              print(selected);
+            });
+          },
+        ),
+      ],
+    );
+  }
+}
+
 class _MyHomePageState extends State<MyHomePage> {
   // int _counter = 0;
 
@@ -282,7 +344,8 @@ class _MyHomePageState extends State<MyHomePage> {
           children: [
             SizedBox(
               height: 300,
-              child: MyTextField(),
+              // child: MyTextField(),
+              child: MyDropDown(),
               // child: MyWrap(),
             //   child: MyListView(),
             ),
