b8dbea61242282e280b3e250c6b76d4b07816ec7
Sidra Febrian Hardiyanto <sidrafhardiyanto@gmail.com>
2025-01-31 14:19:45 +0700

    praktik materi listview

--- aplikasi_flutter_magang/lib/main.dart
+++ aplikasi_flutter_magang/lib/main.dart
@@ -54,6 +54,99 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+class MyListView extends StatelessWidget {
+  MyListView({super.key});
+
+  final List<String> categories = [
+    "All",
+    "Living Room",
+    "Bedroom",
+    "Dining Room",
+    "Kitchen"
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Coding Flutter - ListView"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          SizedBox(
+            height: 35,
+            child: CustomListView(
+              categories: categories,
+              scrollDirection: Axis.horizontal,
+            ),
+          ),
+          SizedBox(
+            height: 200,
+            child: CustomListView(
+              categories: categories,
+              scrollDirection: Axis.vertical,
+            ),
+          ),
+          SizedBox(
+            height: 200,
+            child: CustomListView(
+              categories: categories,
+              scrollDirection: Axis.vertical,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomListView extends StatelessWidget {
+  const CustomListView({
+    super.key,
+    required this.categories,
+    required this.scrollDirection,
+  });
+
+  final List<String> categories;
+  final Axis scrollDirection;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      scrollDirection: scrollDirection,
+      children: List.generate(
+        categories.length,
+        (index) {
+          return GestureDetector(
+            onTap: () {},
+            child: AnimatedContainer(
+              duration: const Duration(milliseconds: 150),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.grey,
+              ),
+              padding: const EdgeInsets.symmetric(
+                horizontal: 24,
+                vertical: 8,
+              ),
+              margin: const EdgeInsets.symmetric(horizontal: 2, vertical: 2),
+              child: Text(categories[index],
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    fontSize: 12,
+                    color: Colors.black,
+                    fontWeight: FontWeight.w500,
+                    letterSpacing: 1,
+                  )),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
 class _MyHomePageState extends State<MyHomePage> {
   // int _counter = 0;
 
@@ -170,20 +263,20 @@ class _MyHomePageState extends State<MyHomePage> {
               radius: 50,
               backgroundImage: NetworkImage('https://picsum.photos/200'),
             ),
-            Row(
-              children: [
-                SizedBox(
-                  width: 180,
-                  child: ClipRRect(
-                    borderRadius: const BorderRadius.only(
-                      topLeft: Radius.circular(20),
-                      topRight: Radius.circular(20),
-                    ),
-                    child: Image.asset('assets/images/furniture/img_product_1.jpg'),
-                  ),
-                ),
-              ],
-            ),
+            // Row(
+            //   children: [
+            //     SizedBox(
+            //       width: 180,
+            //       child: ClipRRect(
+            //         borderRadius: const BorderRadius.only(
+            //           topLeft: Radius.circular(20),
+            //           topRight: Radius.circular(20),
+            //         ),
+            //         child: Image.asset('assets/images/furniture/img_product_1.jpg'),
+            //       ),
+            //     ),
+            //   ],
+            // ),
           ],
         ), 
       ),
