f7b44ed9f65365fb5bbc3d5d9cb01e35a92a68fb
Sidra Febrian Hardiyanto <sidrafhardiyanto@gmail.com>
2025-02-04 12:59:06 +0700

    menambahkan asset icon dan image, juga appbar

--- flutter_ui_figma/lib/main.dart
+++ flutter_ui_figma/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,7 +33,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const HomePage(),
     );
   }
 }
@@ -52,6 +54,40 @@ class MyHomePage extends StatefulWidget {
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        leading: IconButton(
+          onPressed: () {},
+          icon: SvgPicture.asset('assets/icons/menu.svg'),
+        ),
+        title: Center(
+            child: Text(
+          "Home",
+          style: GoogleFonts.poppins(
+            textStyle: const TextStyle(
+                fontWeight: FontWeight.w600,
+                fontSize: 16,
+                color: Color(0xff4A4543)),
+          ),
+        )),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: SvgPicture.asset('assets/icons/search.svg'),
+          ),
+        ],
+      ),
+      
+    );
+  }
 }
 
 class _MyHomePageState extends State<MyHomePage> {
